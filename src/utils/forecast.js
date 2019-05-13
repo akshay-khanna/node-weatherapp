9b7fdfefa881805be8d9e089be674bb16a7d35b2
akshay khanna <akshaykhanna2493@gmail.com>
2019-05-14 00:49:24 +0530

    Low high Temp and Sunrise Sunset updates

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@
 
 const request=require('request') 
 const forecast=(latitude,longitude,callback)=>{
-const url='https://api.darksky.net/forecast/aad0b0371bc6e87264707ffb1aca7a72/'+encodeURIComponent(latitude)+','+encodeURIComponent(longitude)
+const url='https://api.darksky.net/forecast/aad0b0371bc6e87264707ffb1aca7a72/'+encodeURIComponent(latitude)+','+encodeURIComponent(longitude)+'?units=si'
 console.log(url);
 request({ url, json: true} ,(error,{body})=>{
     if(error){
@@ -24,13 +24,16 @@ request({ url, json: true} ,(error,{body})=>{
         
         const {currently}=body
         const{daily:today}=body
-
+        let sunrise=new Date(today.data[0].sunriseTime*1000)
+        let sunset=new Date(today.data[0].sunsetTime*1000)
+        sunrise=sunrise.getHours()+":"+sunrise.getMinutes()+":"+sunrise.getSeconds()
+        sunset=sunset.getHours()+":"+sunset.getMinutes()+":"+sunset.getSeconds()
         // callback(undefined,{
         //     summary:response.body.daily.data[0].summary,
         //     temp:response.body.currently.temperature ,
         //     precProob:response.body.currently.precipProbability
         // })
-        callback(undefined,today.data[0].summary +".It is currently " + currently.temperature + " degrees out.There is a "+currently.precipProbability+"% chance of rain.")
+        callback(undefined,today.data[0].summary +".It is currently " + currently.temperature + " degrees out. The high and low temperatures for today would be  "+ today.data[0].temperatureHigh +" and "+today.data[0].temperatureLow +".Sun Rises at " +sunrise+" and sunset at "+sunset+".There is a "+currently.precipProbability+"% chance of rain.")
     }
     })     
 }
